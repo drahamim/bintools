11931974e3f2b2a69a230eb3869bc59f6a0f2a49
Daniel Rahamim <drahamim@kochava.com>
2022-05-18 17:13:20 -0700

    Working version

--- csrgen.py
+++ csrgen.py
@@ -3,7 +3,13 @@ import argparse
 import fnmatch
 import re
 import os
-from jinja2 import Template
+import subprocess
+import readline
+from jinja2 import Template, Environment, FileSystemLoader
+
+file_loader = FileSystemLoader('.')
+env = Environment(loader=file_loader)
+
 
 description = """
 Simple script to handle generating Certificate Signing Requests (CSR)
@@ -59,7 +65,7 @@ def check_domain(input):
         domain_template = re.compile(
             "^(?=.{1,255}$)(?!-)[A-Za-z0-9\-]{1,63}(\.[A-Za-z0-9\-]{1,63})*\.?(?<!-)$")
         if domain_template.match(input):
-            return input
+            return input.lower()
         else:
             raise argparse.ArgumentError(
                 "Unable to determine if this is a domain or a file. Please check your input.")
@@ -89,26 +95,88 @@ def parse_arguments():
         parser.print_help()
         exit()
 
-def gen_csr():
-    print("Generating CSR..")
+
+def gen_csr_config(args, config_file):
     
+    tm = env.get_template(config_file)
+    print(config_file)
+    SANS = list()
+    print("Generating CSR configuration..")
+    if type(args.domain) is tuple:
+        domain_file, is_file = args.domain
+        with open(domain_file) as fp:
+            domain = fp.readline()
+            count = 0
+            while domain:
+                print(domain)
+                if count == 0:
+                    CN = domain.strip().lower()
+                    domain = fp.readline()
+                    count += 1
+                elif 1 <= count <= 100:
+                    SANS.append(domain.strip().lower())
+                    domain = fp.readline()
+                    count += 1
+                else:
+                    print("Too many domains! Please limit to 100")
+                    exit(1)
+            print(CN)
+    else:
+        domain = args.domain
+        CN = domain.strip().lower()
+
+    generated_csr_config = open(args.domain + '_csr_config', 'w')
+    generated_csr_config.write(tm.render(cn=CN, sans=SANS))
+    generated_csr_config.close()
+    
+    return str(args.domain + '_csr_config')
+
 
 def main(args):
-    # Look for presence of 1 or more config files
     if args.command == 'csr':
+        # Look for csrgen config files
         config_file_search = fnmatch.filter(os.listdir(), "*_gen_config")
         if len(config_file_search) == 0:
             print("No config files found please run with 'config -h' to create one")
             exit()
         elif len(config_file_search) > 1:   # Check if there is more then one config file
             if args.org:
+                # Check if any of the config files match the specified Org
                 if check_file({args.org + "_gen_config"}):
-                    gen_csr()
+                    csr_config_out = gen_csr_config(
+                        args, {args.org + "_gen_config"})
             else:
                 print("Please specify Organization name with -o <Org>")
                 exit()
-        else:  # there is only one found so assume its the right one
-            gen_csr()
+        else:  # There is only one found so assume its the right one\
+            csr_config_out = gen_csr_config(args, config_file_search[0])
+
+
+        # Validate key argument and file
+        if args.key:
+            if check_file(args.key):
+                try:
+                    subprocess.call(['openssl', 'rsa', '-in',
+                                    args.key, '-check'])
+                except:
+                    print("Key specified is not a valid rsa key.")
+            else:
+                print("Key file specified doesn't exist.")
+        else:
+            print("No key specified. Creating a new one.")
+            subprocess.call(['openssl', 'genrsa', '-out',
+                             args.domain + '.key', '2048'])
+            print("Key Generated")
+
+        
+        if args.key:
+            print("Generatinc CSR")
+            subprocess.call(['openssl', 'req', '-new', '-config', csr_config_out,
+                             '-key',  + args.key, '-out', args.domain + '.csr'])
+        else:
+            print("Generatinc CSR")
+            subprocess.call(['openssl', 'req', '-new', '-config', csr_config_out,
+                             '-key', args.domain + '.key', '-out', args.domain + '.csr'])
 
     elif args.command == 'config':
         tm = Template(config_template)
